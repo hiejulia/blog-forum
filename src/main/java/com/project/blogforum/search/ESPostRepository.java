c3276ed89ef2da1ba6780c699b51cdaa70949112
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-04-10 13:08:46 +0300

    config elasticSearch

--- src/main/java/com/project/blogforum/search/ESPostRepository.java
+++ src/main/java/com/project/blogforum/search/ESPostRepository.java
@@ -1,20 +1,20 @@
-package com.project.blogforum.repository.es;
-
-
+package com.project.blogforum.search;
 
 import com.project.blogforum.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 
 public interface ESPostRepository extends ElasticsearchRepository<Post, Long> {
 
     // Find post by author
     Page<Post> findByAuthor(String author, Pageable pageable);
 
-    // Find post by title 
+    // Find post by title
     List<Post> findByTitle(String title);
 
 }
