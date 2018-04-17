ab6d9c605d6fd139f10575e18799913e09d0d60c
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-04-17 22:15:57 +0300

    init test

---
+++ src/test/java/com/project/blogforum/service/CustomRabbitMQMessagingTest.java
@@ -0,0 +1,19 @@
+package com.project.blogforum.service;
+
+
+import org.apache.kafka.clients.producer.internals.Sender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class CustomRabbitMQMessagingTest {
+
+    @Test
+    public void hello() throws Exception {
+
+    }
+}
